0532029d971a138bb7d310427843cee14f4b07e0
jackcaca <jackcaca0519@yahoo.com.tw>
2024-01-26 11:33:01 +0800

    adjust default value

--- api/prompt.py
+++ api/prompt.py
@@ -8,7 +8,7 @@ LANGUAGE_TABLE = {
   "en": "Hello!"
 }
 
-AI_GUIDELINES = '你是一AI生活小幫手，可以回答使用者所有問題'
+# AI_GUIDELINES = '你是一AI生活小幫手，可以回答使用者所有問題'
 
 class Prompt:
     def __init__(self):
@@ -16,7 +16,7 @@ class Prompt:
         self.msg_list.append(
             {
                 "role": "system", 
-                "content": f"{LANGUAGE_TABLE[chat_language]}, {AI_GUIDELINES})"
+                "content": f"{LANGUAGE_TABLE[chat_language]}"
              })    
     def add_msg(self, new_msg):
         if len(self.msg_list) >= MSG_LIST_LIMIT:
